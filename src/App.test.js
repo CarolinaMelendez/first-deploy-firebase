756fcc9959c955a5619c83cd276dab2d938b95b1
Carolina Melendez <venecia.melendez@ucb.edu.bo>
2022-08-26 01:47:22 -0400

    added new test

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('greeting text', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Hi there/);
+  expect(linkElement).toBeInTheDocument();
+});
